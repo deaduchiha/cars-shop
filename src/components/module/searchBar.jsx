1f39ebb1b840266e58603b9e7df06d223e08e0c0
Alireza Nikzad <106245596+deaduchiha@users.noreply.github.com>
2023-03-04 13:57:06 +0330

    add onChange and router for search

--- src/components/module/searchBar.jsx
+++ src/components/module/searchBar.jsx
@@ -1,13 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./searchBar.module.css";
+import { useRouter } from "next/router";
 export const SearchBar = () => {
+  const router = useRouter();
+  const [min, setMin] = useState("");
+  const [max, setMax] = useState("");
+
+  const searchHandler = () => {
+    if (min && max) {
+      router.push(`/filter/${min}/${max}`);
+    } else {
+      alert("Enter maximum or minimum price!");
+    }
+  };
   return (
     <div className={styles.container}>
       <div>
-        <input type="text" placeholder="Inter min-price" />
-        <input type="text" placeholder="Inter max-price" />
+        <input
+          value={min}
+          type="text"
+          placeholder="Inter min-price"
+          onChange={(e) => setMin(e.target.value)}
+        />
+        <input
+          value={max}
+          type="text"
+          placeholder="Inter max-price"
+          onChange={(e) => setMax(e.target.value)}
+        />
       </div>
-      <button>Search</button>
+      <button onClick={searchHandler}>Search</button>
     </div>
   );
 };
